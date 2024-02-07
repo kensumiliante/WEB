8c10f488aeb77997af47945dfc3125e1d387fa25
Kensu <kensumiliante@outlook.com>
2024-02-07 13:51:36 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,14 +191,19 @@ document.onkeydown = function (e) {
 }
 
 // Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/chat/65c376650ff6374032ca41f9/1hm1od00r';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65c376650ff6374032ca41f9/1hm1od00r';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+
 })();
 // End of Tawk.to Live Chat
 
