d0b2ac095441947b0c55cb67cf90bbf2051e99b3
Kensu <kensumiliante@outlook.com>
2024-04-15 15:54:36 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -191,6 +191,7 @@ document.onkeydown = function (e) {
 }
 
 /*<!--Start of Tawk.to Script--> */
+
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -202,6 +203,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
+
 /*<!--End of Tawk.to Script-->  */
 
 
